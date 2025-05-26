ab2665c6235f2f23e5dab178dd60e1e56a5df146
Takahiro Ikeuchi <takahiro.ikeuchi@awarefy.com>
2025-05-27 00:19:34 +0900

    feat: implement gRPC handlers and mappers for Todo service with CRUD operations

---
+++ internal/presentation/grpc/handler/todohandler/todo_mapper.go
@@ -0,0 +1,63 @@
+package todohandler
+
+import (
+	"github.com/google/uuid"
+	"github.com/iktakahiro/oniongo/internal/domain/todo"
+	pb "github.com/iktakahiro/oniongo/internal/infrastructure/grpc/gen/oniongo/v1"
+)
+
+// domainTodoToProto converts a domain Todo to a protobuf Todo
+func domainTodoToProto(domainTodo *todo.Todo) *pb.Todo {
+	pbTodo := &pb.Todo{
+		Id:        domainTodo.ID().String(),
+		Title:     domainTodo.Title(),
+		Body:      domainTodo.Body(),
+		Status:    domainStatusToProtoStatus(domainTodo.Status()),
+		CreatedAt: domainTodo.CreatedAt().Unix(),
+		UpdatedAt: domainTodo.UpdatedAt().Unix(),
+	}
+
+	if completedAt := domainTodo.CompletedAt(); completedAt != nil {
+		timestamp := completedAt.Unix()
+		pbTodo.CompletedAt = &timestamp
+	}
+
+	return pbTodo
+}
+
+// domainStatusToProtoStatus converts a domain TodoStatus to a protobuf TodoStatus
+func domainStatusToProtoStatus(domainStatus todo.TodoStatus) pb.TodoStatus {
+	switch domainStatus {
+	case todo.TodoStatusNotStarted:
+		return pb.TodoStatus_TODO_STATUS_NOT_STARTED
+	case todo.TodoStatusInProgress:
+		return pb.TodoStatus_TODO_STATUS_IN_PROGRESS
+	case todo.TodoStatusCompleted:
+		return pb.TodoStatus_TODO_STATUS_COMPLETED
+	default:
+		return pb.TodoStatus_TODO_STATUS_UNSPECIFIED
+	}
+}
+
+// protoStatusToDomainStatus converts a protobuf TodoStatus to a domain TodoStatus
+func protoStatusToDomainStatus(protoStatus pb.TodoStatus) todo.TodoStatus {
+	switch protoStatus {
+	case pb.TodoStatus_TODO_STATUS_NOT_STARTED:
+		return todo.TodoStatusNotStarted
+	case pb.TodoStatus_TODO_STATUS_IN_PROGRESS:
+		return todo.TodoStatusInProgress
+	case pb.TodoStatus_TODO_STATUS_COMPLETED:
+		return todo.TodoStatusCompleted
+	default:
+		return todo.TodoStatusNotStarted
+	}
+}
+
+// parseUUIDFromString parses a UUID string and returns a TodoID
+func parseUUIDFromString(idStr string) (todo.TodoID, error) {
+	id, err := uuid.Parse(idStr)
+	if err != nil {
+		return todo.TodoID{}, err
+	}
+	return todo.TodoID(id), nil
+}
